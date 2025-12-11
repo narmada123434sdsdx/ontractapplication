b79131fea80b13e5ff92397d4c0ebd53cc49ca0a
RaviKumar0509 <narmadar710@gmail.com>
2025-12-11 14:57:42 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
