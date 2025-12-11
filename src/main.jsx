5c7ec89b6438eb33223d7a5beba349ff67464d02
RaviKumar0509 <narmadar710@gmail.com>
2025-12-11 15:53:57 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+// Optional: Bootstrap CSS (can remove if using only CDN)
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
